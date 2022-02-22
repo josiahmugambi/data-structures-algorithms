30e20b27e95cf4e16a7c04bbf320b6921bae259d
Josiah Mugambi <jmugambi@gmail.com>
2022-02-22 18:30:17 +0300

    #5 'fibonacci number again' done

---
+++ Week2-AlgorithmicWarmup/5-fibonacci_number_again/fibonacci_number_again_unit_tests.py
@@ -0,0 +1,39 @@
+# python3
+
+
+def fibonacci_number_again_naive(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % m
+    return current
+
+
+def p_period_length(m):
+    previous, current = 0, 1
+    for i in range(0, m**2):
+        previous, current = current, (previous + current) % m
+        if previous == 0 and current == 1:
+            return i + 1
+
+
+def fibonacci_number_again(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+    p_period = p_period_length(m)
+    n = n % p_period
+    if n <= 1:
+        return n
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)
+    return current % m
+
+
+if __name__ == '__main__':
+    input_n, input_m = map(int, input().split())
+#    print(fibonacci_number_again_naive(input_n, input_m))
+    print(fibonacci_number_again(input_n, input_m))
