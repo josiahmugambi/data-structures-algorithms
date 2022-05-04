34e944cc1e959b2b9676d9a51e05c2eb6f7a208a
Josiah Mugambi <jmugambi@gmail.com>
2022-05-04 21:39:27 +0300

    dequeing a network work sim

--- 2_data-structures/week1_basic_data_structures/3_network_simulation/process_packages.py
+++ 2_data-structures/week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -1,6 +1,7 @@
 # python3
 
 from collections import namedtuple
+from collections import deque
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
@@ -9,13 +10,35 @@ Response = namedtuple("Response", ["was_dropped", "started_at"])
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish_time = []
+        self.finish_time = deque()
+
+#    def process(self, request):
+#        # write your code here
+#        return Response(False, -1)
 
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        
+        # remove processed items from buffer
+        while len(self.finish_time) > 0:
+            if self.finish_time[0] <= request.arrived_at:
+                self.finish_time.popleft()
+            else:
+                break
+        
+        # full buffer
+        if len(self.finish_time) >= self.size:
+            return Response (True, -1)
 
-
+        #Empty buffer
+        if len(self.finish_time) == 0:
+            self.finish_time.append(request.arrived_at + request.time_to_process)
+            return Response(False, request.arrived_at)
+        
+        last_finish_time = self.finish_time[-1]
+        self.finish_time.append(last_finish_time + request.time_to_process)
+        
+        return Response(False, last_finish_time) 
+    
 def process_requests(requests, buffer):
     responses = []
     for request in requests:
