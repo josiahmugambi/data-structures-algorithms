1d7bae4e6474d00dbaa8b38eccfcc0b06a6eae80
Josiah Mugambi <jmugambi@gmail.com>
2022-05-15 21:40:27 +0300

    week 2 in progress

--- 2_data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ 2_data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,5 +1,6 @@
 # python3
 
+from collections import deque
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
@@ -8,11 +9,22 @@ def max_sliding_window_naive(sequence, m):
 
     return maximums
 
+def max_sliding_window(sequence, m):
+    n = len(sequence)
+    window = deque(sequence[:m])
+    maxslide = []
+    for i in sequence[m:n]:
+        maxslide.append(max(window))
+        window.popleft()
+        window.append(i)
+    maxslide.append(max(window))    
+    return maxslide
+
 if __name__ == '__main__':
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
+    print(*max_sliding_window(input_sequence, window_size))
 
--- 2_data-structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ 2_data-structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,7 +1,43 @@
 # python3
+swaps = []
+data = []
+size = 0
 
+def left_child(i):
+    return 2*i
+
+def right_child(i):
+    return 2*i + 1
+
+def sift_down(i):
+    global size
+    global data
+    global swaps
+    
+    min_index = i 
+    
+    l = left_child(i) 
+    if l <= size and data[l - 1] < data[min_index - 1]:
+        min_index = l
+
+    r = right_child(i)    
+    if r <= size and data[r - 1] < data[min_index - 1]:
+        min_index = r
+    
+    if i != min_index:
+        data[i - 1], data[min_index - 1] = data[min_index - 1], data[i - 1]
+        sift_down(min_index)
+        swaps.append([i - 1, min_index - 1])
 
 def build_heap(data):
+    global size
+    for i in range (size//2, 0, -1):
+        sift_down(i)
+    
+    return swaps
+
+
+def build_heap_naive(data):
     """Build a heap from ``data`` inplace.
 
     Returns a sequence of swaps performed by the algorithm.
@@ -22,10 +58,15 @@ def build_heap(data):
 
 
 def main():
+    global data
+    global swaps
+    global size
+
     n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
 
+    size = len(data)
     swaps = build_heap(data)
 
     print(len(swaps))
