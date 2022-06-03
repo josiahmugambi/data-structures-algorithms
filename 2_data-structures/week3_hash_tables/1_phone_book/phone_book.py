42c65d9260ac043a79aee7fd68286d48885672f2
Josiah Mugambi <jmugambi@gmail.com>
2022-06-03 15:25:36 +0300

    chains

--- 2_data-structures/week3_hash_tables/1_phone_book/phone_book.py
+++ 2_data-structures/week3_hash_tables/1_phone_book/phone_book.py
@@ -1,15 +1,15 @@
 # python3
 
-class Query:
+class PhoneBook:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
+        
 def read_queries():
     n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    return [PhoneBook(input().split()) for i in range(n)]
 
 def write_responses(result):
     print('\n'.join(result))
@@ -17,31 +17,30 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    
+    phone_book = [None] * 10**7
+    
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            
+          #  if phone_book[cur_query.number] != None:
+            phone_book[cur_query.number] = cur_query.name
+
+            #else:
+
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            
+            phone_book[cur_query.number] = None
+            
         else:
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if phone_book[cur_query.number] != None:
+                response = phone_book[cur_query.number]
+
             result.append(response)
     return result
 
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
