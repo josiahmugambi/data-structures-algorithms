e349b84202507cfe26908f7bac1d69317625fd1a
Josiah Mugambi <jmugambi@gmail.com>
2022-06-11 19:46:58 +0300

    Rabin-Karp Algo - Efficient Implementation

--- 2_data-structures/week3_hash_tables/3_hash_substring/.ipynb_checkpoints/hash_substring-checkpoint.py
+++ 2_data-structures/week3_hash_tables/3_hash_substring/.ipynb_checkpoints/hash_substring-checkpoint.py
@@ -1,18 +1,41 @@
 # python3
 
+import random
+
 def read_input():
     return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+def get_occurrences_rk(pattern, text):
+    big_prime = 100000007
+    x = random.randint(1, big_prime - 1)
+    positions = []
+    p_hash = poly_hash(pattern, big_prime, x)
+    for i in range (len(text) - len(pattern) + 1):
+        t_hash = poly_hash(text[i:i + len(pattern)], big_prime, x)
+        if p_hash != t_hash:
+            continue
+
+        if (text[i:i + len(pattern)]) == pattern:
+            positions.append(i)        
+    return positions
+
+def poly_hash(S, p, x):
+    po_hash = 0
+    for i in range(len(S) - 1, -1, -1):
+        po_hash = (((po_hash * x + ord(S[i])) % p) + p)%p
+    return po_hash
+    
+def are_equal(S1, S2):
+    if len(S1) == len(S2):
+        return False
+    for i in range(len(S1)-1):
+        if S1[i] != S2[i]:
+            return False
+    return True
 
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    print_occurrences(get_occurrences_rk(*read_input()))
 
--- 2_data-structures/week3_hash_tables/3_hash_substring/hash_substring.py
+++ 2_data-structures/week3_hash_tables/3_hash_substring/hash_substring.py
@@ -1,18 +1,45 @@
 # python3
 
+import random
+
 def read_input():
     return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+def get_occurrences_rk(pattern, text):
+    big_prime = 100000007
+    x = random.randint(1, big_prime - 1)
+    positions = []
+    p_hash = poly_hash(pattern, big_prime, x)
+    H = pre_compute_hashes(text, pattern, big_prime, x)
+    for i in range (len(text) - len(pattern) + 1):
+
+        if p_hash != H[i]:
+            continue
+
+        if (text[i:i + len(pattern)]) == pattern:
+            positions.append(i)        
+    return positions
+
+def poly_hash(S, p, x):
+    po_hash = 0
+    for i in range(len(S) - 1, -1, -1):
+        po_hash = (((po_hash * x + ord(S[i])) % p) + p) % p
+    return po_hash
+
+def pre_compute_hashes(text, pattern, p, x):
+    H = [0] * (len(text) - len(pattern) + 1)
+    S = text[len(text) - len(pattern): len(text)  ]
+    H[len(text) - len(pattern)] = poly_hash(S, p, x)
+    y = 1
+    for i in range(0, len(pattern)):
+        y = (y * x) % p
+    for i in range(len(text) - len(pattern) - 1, -1, -1):
+        H[i] = (x*H[i+1] + ord(text[i]) - y*ord(text[i + len(pattern)])) % p
+    return H
 
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    print_occurrences(get_occurrences_rk(*read_input()))
 
