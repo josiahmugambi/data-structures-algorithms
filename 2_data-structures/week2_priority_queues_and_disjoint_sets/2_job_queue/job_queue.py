391315c5adb9c9e37378e4629283cfe520629b30
Josiah Mugambi <jmugambi@gmail.com>
2022-07-06 22:50:37 +0300

    week 3 done

--- 2_data-structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ 2_data-structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,32 +1,64 @@
 # python3
 
-from collections import namedtuple
-
-AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
-
-
-def assign_jobs(n_workers, jobs):
-    # TODO: replace this code with a faster algorithm.
-    result = []
-    next_free_time = [0] * n_workers
-    for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
-
-    return result
-
-
-def main():
-    n_workers, n_jobs = map(int, input().split())
-    jobs = list(map(int, input().split()))
-    assert len(jobs) == n_jobs
-
-    assigned_jobs = assign_jobs(n_workers, jobs)
-
-    for job in assigned_jobs:
-        print(job.worker, job.started_at)
-
+class ParallelProcessing:
+        
+    def read_data(self):
+        #self.n_workers, m_jobs = (4,20)
+        #self.job_durations = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+        self.n_workers, m_jobs = map(int, input().split())
+        self.job_durations = list(map(int, input().split()))
+        assert m_jobs == len(self.job_durations)
+    
+    def left_child(self, i):
+  #      return 2*i + 1
+        return 2*i
+    
+    def right_child(self, i):
+#        return 2*i + 2
+        return 2*i + 1
+    
+    def parent(self, i):
+        return (i - 1) //2
+        
+    def sift_down(self, i):
+        size = len(self.nodes) - 1
+        min_index = i 
+        l = self.left_child(i) 
+        r = self.right_child(i)  
+        
+        if l <= size:
+            if self.nodes[l][1] < self.nodes[min_index][1]:
+                min_index = l
+            elif self.nodes[l][1] == self.nodes[min_index][1] and self.nodes[l][0] < self.nodes[min_index][0]:
+                min_index = l
+                
+        if r <= size:
+            if self.nodes[r][1] < self.nodes[min_index][1]:
+                min_index = r
+            elif self.nodes[r][1] == self.nodes[min_index][1] and self.nodes[r][0] < self.nodes[min_index][0]:
+                min_index = r 
+        
+        if i != min_index:
+            self.nodes[i], self.nodes[min_index] = self.nodes[min_index], self.nodes[i]
+            self.sift_down(min_index)
+        
+    def assign_jobs(self):
+        self.assigned_worker = [None] * len(self.job_durations)
+        self.start_time = [None] * len(self.job_durations)
+        #self.assigned_job = namedtuple("AssignedJob", ["worker", "started_at"])
+        self.nodes = [None] + [[x,0] for x in range(self.n_workers)]
+        for d in range (len(self.job_durations)):
+            self.assigned_worker[d] = self.nodes[1][0]
+            self.start_time[d] = self.nodes[1][1]
+            self.nodes[1][1] += self.job_durations[d]
+            self.sift_down(1)
+        
+    def print_result(self):
+        for j in range(len(self.job_durations)):
+            print(self.assigned_worker[j], self.start_time[j])
 
 if __name__ == "__main__":
-    main()
+    pp = ParallelProcessing()
+    pp.read_data()
+    pp.assign_jobs()
+    pp.print_result()
