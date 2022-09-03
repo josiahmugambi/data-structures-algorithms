a23f4c692253a29573d384b92cc2a7b1fc46d72f
Josiah Mugambi <jmugambi@gmail.com>
2022-09-03 13:26:57 +0300

    wk 2 - 2

--- 3_algorithms-on-graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
+++ 3_algorithms-on-graphs/week2_graph_decomposition2/2_order_of_courses/toposort.py
@@ -2,21 +2,31 @@
 
 import sys
 
-def dfs(adj, used, order, x):
+def dfs(adj, visited, order, x):
     #write your code here
-    pass
+    visited[x] = True
+    if len(adj[x]) > 0:
+        for w in adj[x]:
+            if not visited[w]:
+                dfs(adj, visited, order, w)
+    order.append(x)
+    #return order
 
 
 def toposort(adj):
-    used = [0] * len(adj)
+    visited = [False] * len(adj)
     order = []
     #write your code here
+    for v in range(len(adj)):
+        if not visited[v]:
+            dfs(adj, visited, order, v)
+    order = order[::-1]
     return order
 
 if __name__ == '__main__':
-    #input = sys.stdin.read()
-    #data = list(map(int, input.split()))
-    data = [4,3,1,2,4,1,3,1]
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    #data = [4,3,1,2,4,1,3,1]
     #data = [4,1,3,1]
     #data = [5,7,2,1,3,2,3,1,4,3,4,1,5,2,5,3]
     n, m = data[0:2]
